27901ad1029bce63e757b065557026e111272135
Meera A <mirunaalini.alagarajah@gmail.com>
2023-10-12 10:57:00 +0100

    website compiles

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,25 @@
 import React,{ useState }  from 'react';
 import '../styles/search.css'
+import getImages from '../requests/getImages';
 
 const Search=()=>{
 const[value,setValue]=useState();
 
+const handleSubmit=(event)=>{
+    event.preventDefault();
+    getImages(value);
+};
+
+
 
     return (
-        <>
-        <form className="form">
+        <div className="search">
+        <form className="form" onSubmit={handleSubmit}>
         <input className="searchBox" type="text" onChange={(e)=> setValue(e.target.value)}/>
 
         <button className="button" type="submit">SEARCH</button>
         </form>
-        </>
+        </div>
     );
 };
 
